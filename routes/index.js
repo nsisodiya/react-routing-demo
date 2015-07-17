7601946ca4008ad07e268d83179df99cd9d7e61d
Narendra Sisodiya <narendra@narendrasisodiya.com>
2015-07-18 00:59:58 +0530

    lots of movement

--- routes/index.js
+++ routes/index.js
@@ -5,9 +5,6 @@ router.get("*", function(req, res, next) {
 	console.log("req Generic");
 	console.log(req.baseUrl);
 	res.render('index', { title: 'Express' });
-//	var path = __dirname + '/../../client/index.html';
-//	console.log(path);
-//	res.sendfile(path);
 });
 
 module.exports = router;
